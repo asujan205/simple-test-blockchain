1b6f07de77af74c76d4187f381e4ef5664ec3c39
asujan205 <asujan205@gmail.com>
2023-09-13 22:33:25 +0545

    #changes

--- src/config/wallets.ts
+++ src/config/wallets.ts
@@ -1,13 +1,29 @@
-import { InjectedConnector } from "@web3-react/injected-connector";
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
 
-export const ChainId = {
-  ETHEREUM: 1,
-  BSC: 56,
-  BSC_TESTNET: 97,
+export const connectWallet = async (): Promise<{
+  address: string;
+}> => {
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const obj = {
+        address: addressArray[0],
+      };
+      return obj;
+    } catch (err) {
+      return {
+        address: "",
+      };
+    }
+  } else {
+    return {
+      address: "",
+    };
+  }
 };
-
-const supportedChainIds = Object.values(ChainId);
-
-export const injected = new InjectedConnector({
-  supportedChainIds,
-});
